44cdc9db1af5ce81a8a3a506e7602927554945f1
Swiffels <54492510+Swiffels@users.noreply.github.com>
2024-09-09 16:31:42 -0600

    Reapply "fixed finally?"
    This reverts commit b570fd42300e3fc3542b9f8711886f72e91a9342.

--- src/Hero/ThreeImports.jsx
+++ src/Hero/ThreeImports.jsx
@@ -5,7 +5,7 @@ const importModel = (name, scene, callback) => {
   const loader = new GLTFLoader();
 
   loader.load(
-    `public/Assets/${name}.gltf`,
+    `assets/${name}.gltf`,
     function (gltf) {
       gltf.scene.scale.set(30, 30, 30);
       scene.add(gltf.scene);
