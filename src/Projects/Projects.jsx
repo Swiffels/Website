b570fd42300e3fc3542b9f8711886f72e91a9342
Swiffels <54492510+Swiffels@users.noreply.github.com>
2024-09-09 16:28:31 -0600

    Revert "fixed finally?"
    This reverts commit edb5ce1e94c981650a95e1f790f8a6d247067daf.

--- src/Projects/Projects.jsx
+++ src/Projects/Projects.jsx
@@ -5,9 +5,9 @@ function Projects() {
     <div className="w-full bg-slate-100 text-center">
       <h1 className="text-raisin_black-500 font-custom font-extrabold text-5xl pt-8 pb-10">Featured Projects</h1>
       <div className="flex justify-around flex-col md:flex-row">
-        <ProjectButton name={"Portfolio"} image={"assets/websiteProjectLogo.png"} description={"Making a website"} />
-        <ProjectButton name={"IoT Alarm Clock"} image={"assets/alarmClockLogo.png"} description={"Webserver-based alarm clock"} />
-        <ProjectButton name={"Accessible Rock Climbing"} image={"assets/rockClimbingLogo.jpg"} description={"Visually impaired climbing holds"} />
+        <ProjectButton name={"Portfolio"} image={"public/assets/websiteProjectLogo.png"} description={"Making a website"} />
+        <ProjectButton name={"IoT Alarm Clock"} image={"public/assets/alarmClockLogo.png"} description={"Webserver-based alarm clock"} />
+        <ProjectButton name={"Accessible Rock Climbing"} image={"public/assets/rockClimbingLogo.jpg"} description={"Visually impaired climbing holds"} />
       </div>
     </div>
   );
