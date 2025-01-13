3357dd21d204238b01fc1f83139a5bee37b7bf77
Swiffels <swp9gaming@gmail.com>
2025-01-12 22:15:25 -0700

    flipped timeline events

--- src/Experience/TimelineEvents.js
+++ src/Experience/TimelineEvents.js
@@ -5,7 +5,7 @@ const experience = [
     date: "Dec 2024 - Present",
     description:
       "Working with a team of five under a professor's guidance to develop a reliable grade migration system for Colorado School of Mines. This service enhances workflows for teachers, students, and TAs by streamlining grade transfers across platforms. Employing REST API architecture with secure user authentication via Authentik and full CRUD functionality. Developed using agile sprints, containerized Spring Boot (Java), PostgreSQL, React-Typescript, and CI/CD pipelines on Microsoft Azure. 🚀",
-    direction: "right",
+    direction: "left",
   },
   {
     heading: "ARIA Lab, School of Mines",
@@ -13,35 +13,35 @@ const experience = [
     date: "Sep 2024 - Present",
     description:
       "Contributing as an undergraduate researcher in ARIA Lab to create an RTK GPS system for robotics projects. Currently integrating it with a Spot robot to support testing of various SLAM algorithms. 📡",
-    direction: "left",
+    direction: "right",
   },
   {
     heading: "Colorado School of Mines, Computer Science",
     subheading: "Artificial Intelligence and Robotics",
     date: "Aug 2024 - Present",
     description: "Pursuing a Bachelor's degree in Computer Science at CSM with a concentration in Artificial Intelligence and Robotics! 🖥",
-    direction: "right",
+    direction: "left",
   },
   {
     heading: "Code Ninjas",
     subheading: "Coding Instructor",
     date: "Dec 2019 - Present",
     description: "Teaching kids how to code in JavaScript, C#, Lua, and Python. Additionally developed game servers to host parties and events for Code Ninjas. 👨‍💻",
-    direction: "left",
+    direction: "right",
   },
   {
     heading: "Red Rocks Community College",
     subheading: "Associate of Engineering",
     date: "Aug 2023 - Aug 2024",
     description: "Attended Red Rocks Community College to advance my education in engineering and computer science. 🎓",
-    direction: "right",
+    direction: "left",
   },
   {
     heading: "Warren Tech Game Development",
     subheading: "Game Programming and Development",
     date: "Aug 2021 - May 2022",
     description: "Participated in Warren Tech's Game Development course, working with a team to create games using Unreal Engine and Unity. 🎮",
-    direction: "left",
+    direction: "right",
   },
 ];
 
